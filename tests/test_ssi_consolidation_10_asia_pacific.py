96ac076fa80a85d48c0f600b2cd2463cc6db1781
Olaniyi Oladokun <oladokunifab@gmail.com>
2026-09-20 13:54:17 +0100

    data: add Bank of China Hong Kong SSI routes

--- tests/test_ssi_consolidation_10_asia_pacific.py
+++ tests/test_ssi_consolidation_10_asia_pacific.py
@@ -15,6 +15,7 @@ EXPECTED_COUNTS = {
     "COMMHKHHXXX": 16,
     "SUNYTWTPXXX": 20,
     "UOVBHKHHXXX": 14,
+    "BKCHHKHHXXX": 14,
 }
 EXPECTED_SOURCES = {
     "https://www.bsp.com.pg/help/rates-and-fees/swift-codes/",
@@ -23,6 +24,7 @@ EXPECTED_SOURCES = {
     "https://www.bankcomm.com.hk/hk/uploadhk/infos/201901/16/2609759/20190116092844_List_of_our_Principal_Correspondent_Banks_en.pdf",
     "https://www.sunnybank.com.tw/public/pdf/1040708-%E9%99%BD%E4%BF%A1%E5%95%86%E6%A5%AD%E9%8A%80%E8%A1%8C%E5%8C%AF%E5%85%A5%E6%AC%BE%E5%85%A5%E5%B8%B3%E6%8C%87%E7%A4%BA%28%E6%89%80%E6%9C%89%E5%B9%A3%E5%88%A5%29.pdf",
     "https://www.uob.com.sg/web-resources/hk/pdf/hk/application-forms/payments-factsheet-inward-to-uob.pdf",
+    "https://www.bochk.com/dam/more/forms/Inward_TT_Quick_Guide_EN.pdf?gc_id=1570305088.1772620853&gs_id=1772620852",
 }
 
 
@@ -34,8 +36,8 @@ def test_asia_pacific_ledger_has_expected_shape_and_counts():
     payload = _payload()
     assert set(payload) == {"source_prs", "banks", "ssi_records"}
     assert payload["source_prs"] == []
-    assert len(payload["banks"]) == len(EXPECTED_COUNTS) == 6
-    assert len(payload["ssi_records"]) == 73
+    assert len(payload["banks"]) == len(EXPECTED_COUNTS) == 7
+    assert len(payload["ssi_records"]) == 87
     assert {bank[0] for bank in payload["banks"]} == set(EXPECTED_COUNTS)
     assert {row[0] for row in payload["ssi_records"]} == set(EXPECTED_COUNTS)
 
