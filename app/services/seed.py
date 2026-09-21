83178d720f7aad5451bd3d4d415a612e053b690e
Olaniyi Oladokun <oladokunifab@gmail.com>
2026-09-21 06:28:32 +0100

    Add Bank of Baroda treasury SSI routes

--- app/services/seed.py
+++ app/services/seed.py
@@ -1366,6 +1366,7 @@ _SSI_CONSOLIDATION_DATA_FILES = (
     "seed_ssi_usbank_fx_fex_20250601.json",
     "seed_ssi_iob_current_deltas_20260120.json",
     "seed_ssi_basic_bank_current_20260720.json",
+    "seed_ssi_bank_of_baroda_treasury_20260921.json",
     "seed_ssi_consolidation_10_south_asia_global_safe.json",
 )
 
