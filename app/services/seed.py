c747ebb3481d7d12e39c1b0432ee86bee17d392f
Olaniyi Oladokun <oladokunifab@gmail.com>
2026-09-21 02:49:17 +0100

    Add Raiffeisen Tirol SSI routes

--- app/services/seed.py
+++ app/services/seed.py
@@ -1342,6 +1342,7 @@ _SSI_CONSOLIDATION_DATA_FILES = (
     "seed_ssi_raiffeisenverband_salzburg_20250501.json",
     "seed_ssi_bankhaus_spaengler_20250301.json",
     "seed_ssi_bks_bank_20260101.json",
+    "seed_ssi_raiffeisenlandesbank_tirol_20260101.json",
     "seed_ssi_consolidation_10_south_asia_global_safe.json",
 )
 
